53f623433a937038a8740af4e6fa87115dcd7611
HDK1218 <i.m.hodaka@violet.tokyo>
2025-07-23 17:53:21 +0900

    feat: add MySalon page and set as default

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
+import MySalon from './pages/MySalon'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<MySalon />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
